c2060537281e6d51fcb9ece53a11f4f515dd6a69
Ziagl <office@ziegelwanger-edv.at>
2024-07-05 21:42:49 +0200

    add createUnit method

--- src/main/UnitManager.ts
+++ src/main/UnitManager.ts
@@ -1,11 +1,15 @@
+import { HexOffset, Orientation, defineHex } from "honeycomb-grid";
+import { IUnit } from "./interfaces/IUnit";
 import { Utils } from "./models/Utils";
 
 export class UnitManager
 {
+    private _unitStore: IUnit[] = [];
     private _map:number[][][] = []; // 2D array with different layers (3rd dimension)
     private _map_layers:number = 0;
     private _map_columns:number = 0;
-    //private _map_rows:number = 0;
+    private _map_rows:number = 0;
+    private _hexDefinition;
 
     constructor(map:number[], layers:number, rows:number, columns:number) {
         // create maps for each possible layer (sea, land,air)
@@ -16,7 +20,48 @@ export class UnitManager
         }
         this._map_layers = layers;
         this._map_columns = columns;
-        //this._map_rows = rows;
+        this._map_rows = rows;
+
+        // initilize definition to convert offset -> cube coordinates
+        const hexSetting = {offset: -1 as HexOffset, orientation: Orientation.POINTY};
+        this._hexDefinition = defineHex(hexSetting);
+    }
+
+    // pass an array to mask all not passable fields (value > 0 == unpassable field)
+    public markNonPassableFields(map:number[], layer:number):boolean {
+        // early exit if data is not correct
+        if(layer < 0 || layer >= this._map_layers || map.length !== this._map_rows * this._map_columns) {
+            return false;
+        }
+        const map2d = Utils.convertTo2DArray(map, this._map_rows, this._map_columns);
+        //  modify map
+        for(let i = 0; i < this._map_rows; ++i) {
+            for(let j = 0; j < this._map_columns; ++j) {
+                // @ts-ignore
+                if(map2d[i][j] !== 0) {
+                    // @ts-ignore
+                    this._map[layer][i][j] = -1;
+                }
+            }
+        }
+        return true;
+    }
+
+    // creates a new unit at given layer, returns false if not possible
+    public createUnit(unit:IUnit, layer:number):boolean {
+        // early exit if layer is not valid
+        if(layer < 0 || layer >= this._map_layers) {
+            return false;
+        }
+        // early exit if layer position is already occupied
+        const index = Utils.getUnitOnPosition(unit.coordinate, this._map[layer]!, this._hexDefinition);
+        if(index !== 0) {
+            return false;
+        }
+
+        // TODO add unit
+
+        return true;
     }
 
     // print generated map structured (one row as one line)
--- src/test/UnitManager.test.ts
+++ src/test/UnitManager.test.ts
@@ -10,3 +10,13 @@ test('initialize', () => {
     expect(output).toContain('Layer 2');
     expect(output).toContain('Layer 3');
 });
+test('markNonPassableFields', () => {
+    const exampleSeaMap:number [] = Array(16).fill(0);
+    const exampleLandMap:number [] = Array(16).fill(1);  
+    const exampleAirMap:number [] = Array(16).fill(2);
+    const unitManager = new UnitManager([...exampleSeaMap, ...exampleLandMap, ...exampleAirMap], 3, 4, 4);
+    const success = unitManager.markNonPassableFields([0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0], 0);
+    expect(success).toBe(true);
+    const output = unitManager.print();
+    expect(output).toContain('0 -1 -1 0');
+});
